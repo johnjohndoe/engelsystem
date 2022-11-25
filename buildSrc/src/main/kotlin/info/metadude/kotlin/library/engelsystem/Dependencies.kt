0cca3adca61b57b98701ed8f81e43a67b9944d32
Tobias Preuss <tobias.preuss@googlemail.com>
2022-11-25 19:01:06 +0100

    Use junit-jupiter v.5.9.1.

--- buildSrc/src/main/kotlin/info/metadude/kotlin/library/engelsystem/Dependencies.kt
+++ buildSrc/src/main/kotlin/info/metadude/kotlin/library/engelsystem/Dependencies.kt
@@ -20,7 +20,7 @@ object GradlePlugins {
 object Libs {
 
     private object Versions {
-        const val junitJupiter = "5.8.2"
+        const val junitJupiter = "5.9.1"
         const val kotlinCoroutines = "1.6.1"
         const val moshi = "1.13.0"
         const val okhttp = "3.12.13"
