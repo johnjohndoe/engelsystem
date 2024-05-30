79c4ad5261da25a25eb52ceabd6123cd2228043e
Tobias Preuss <tobias.preuss@googlemail.com>
2024-05-30 14:14:58 +0200

    Use kotlinx-coroutines-test v.1.8.1.

--- buildSrc/src/main/kotlin/info/metadude/kotlin/library/engelsystem/Dependencies.kt
+++ buildSrc/src/main/kotlin/info/metadude/kotlin/library/engelsystem/Dependencies.kt
@@ -21,7 +21,7 @@ object Libs {
 
     private object Versions {
         const val junitJupiter = "5.10.2"
-        const val kotlinCoroutines = "1.7.3"
+        const val kotlinCoroutines = "1.8.1"
         const val moshi = "1.15.1"
         const val okhttp = "4.12.0"
         const val retrofit = "2.9.0"
