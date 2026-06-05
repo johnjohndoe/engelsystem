80b513b48b63b14aff33f2822f8a68459ce5f8c8
Tobias Preuss <tobias.preuss@googlemail.com>
2026-06-05 11:47:23 +0200

    Use junit v.6.1.0.

--- buildSrc/src/main/kotlin/info/metadude/kotlin/library/engelsystem/Dependencies.kt
+++ buildSrc/src/main/kotlin/info/metadude/kotlin/library/engelsystem/Dependencies.kt
@@ -20,7 +20,7 @@ object GradlePlugins {
 object Libs {
 
     private object Versions {
-        const val junit = "6.0.1"
+        const val junit = "6.1.0"
         const val kotlinCoroutines = "1.11.0"
         const val mockitoKotlin = "6.3.0"
         const val moshi = "1.15.2"
